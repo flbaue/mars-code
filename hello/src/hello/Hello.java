30b0644acba2a6cc1ec0832881499c9268c7437f
Florian Bauer <flbaue@gmail.com>
2013-03-08 00:23:54 +0100

    test

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Florian Bauer
+ * flbaue@gmail.com
+ * Date: 08.03.13
+ * Time: 00:13
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
