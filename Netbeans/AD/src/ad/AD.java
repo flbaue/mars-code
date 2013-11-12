05cfde4e61438e7102c633dbfd12b575a522caa2
Florian Bauer <flo.bauer@gmx.net>
2013-11-12 22:11:12 +0100

    blubb

---
+++ Netbeans/AD/src/ad/AD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ad;
+
+/**
+ *
+ * @author florianbauer
+ */
+public class AD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
