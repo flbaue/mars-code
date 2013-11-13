f91328460bdc4216091f5f5fc00e6031d7eeb1a7
flbaue <flbaue@posteo.de>
2013-11-13 10:06:01 +0100

    blubb

---
+++ HAW/AD/src/counter/StepCounter.java
@@ -0,0 +1,36 @@
+/*
+ * Florian Bauer
+ * flbaue@posteo.de
+ * 2013
+ */
+
+/**
+ * RefCounter.java
+ * Florian Bauer
+ * flbaue@posteo.de
+ * 26.10.2013
+ */
+package counter;
+
+/**
+ * @author Florian Bauer
+ */
+public class StepCounter {
+    private int steps;
+
+    public StepCounter() {
+        steps = 0;
+    }
+
+    public void step() {
+        steps++;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void reset() {
+        steps = 0;
+    }
+}
