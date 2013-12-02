1df33b1b54046052e62b32ab145da74f9e62e655
flbaue <flbaue@posteo.de>
2013-12-02 01:08:01 +0100

    update application layout

--- ruby/rails/first/app/controllers/visitors_controller.rb
+++ ruby/rails/first/app/controllers/visitors_controller.rb
@@ -1,5 +1,8 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
+		flash.now[:warning] = 'Get a present.'
 	end 
 end
